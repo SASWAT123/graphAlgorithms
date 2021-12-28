b3c18400f305687e96a565acc1c708f3d6dc457b
Saswat Priyadarshan <spriyad2@ncsu.edu>
2021-12-28 10:40:51 -0500

    Breadth First Search Traversal

--- src/GraphAlgorithms.java
+++ src/GraphAlgorithms.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class GraphAlgorithms {
 
@@ -9,6 +10,9 @@ public class GraphAlgorithms {
 
     //Visited array to keep track of visited nodes
     List<Integer>visited = new ArrayList<>();
+
+    //Queue to keep track of vertices durin BFS
+    Queue<Integer> queue = new LinkedList<>();
 
     //Number of vertices
     int vertices;
@@ -24,6 +28,19 @@ public class GraphAlgorithms {
 
     public void addEdge(int u, int v){
         adj.get(u).add(v);
+        adj.get(v).add(u);
+    }
+
+    //1. Breadth First Traversal of a graph
+    public void bfs(){
+        while(!queue.isEmpty()){
+            int currentNode = queue.poll();
+            if(visited.get(currentNode) != 1){
+                System.out.print(currentNode + "-> ");
+                visited.set(currentNode, 1);
+                queue.addAll(adj.get(currentNode));
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -32,18 +49,20 @@ public class GraphAlgorithms {
 
         graph.addEdge(0, 1);
         graph.addEdge(0, 2);
-        graph.addEdge(1, 0);
         graph.addEdge(1, 3);
-        graph.addEdge(2, 0);
         graph.addEdge(2, 3);
-        graph.addEdge(3, 1);
-        graph.addEdge(3, 2);
-        graph.addEdge(4, 2);
-        graph.addEdge(4, 3);
+        graph.addEdge(2, 4);
+        graph.addEdge(3, 4);
 
+        System.out.println("Printing the above graph");
         for(int i = 0; i < vertices; i++){
             System.out.println(graph.adj.get(i).toString());
         }
+
+        System.out.println("The BFS traversal of the graph is: ");
+        graph.queue.add(0);
+        graph.bfs();
+
     }
 
 }
